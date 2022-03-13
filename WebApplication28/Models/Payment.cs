47a001b69ce67eba4525eeff34f502e651562c9a
taimaahamarneh <taimaahamarneh1@gmail.com>
2022-03-13 11:18:01 +0300

    Add project files.

---
+++ WebApplication28/Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication28.Models
+{
+    public partial class Payment
+    {
+        public int PaymentId { get; set; }
+        public string PaymentType { get; set; } = null!;
+        public double? PaymentAmount { get; set; }
+        public string CardNumber { get; set; } = null!;
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; } = null!;
+    }
+}
