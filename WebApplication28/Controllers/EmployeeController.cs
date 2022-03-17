53e31d207461dc6dfb854e7e4cce1106d7d0a1cf
taimaahamarneh <taimaahamarneh1@gmail.com>
2022-03-18 00:04:01 +0300

    add admin and employee dashboard controller with view

---
+++ WebApplication28/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication28.Models;
+
+namespace WebApplication28.Controllers
+{
+    public class EmployeeController : Controller
+
+    {
+        private readonly HomeServicesNewContext _context;
+        public EmployeeController(HomeServicesNewContext context) 
+        { 
+            
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
